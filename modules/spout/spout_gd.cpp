52886eec3c5d56ff1022b61aa99e2b584d1f066b
Tim Yuen <timothy.yuen22@gmail.com>
2022-06-09 03:55:32 -0400

    add untested receiver code that still needs to be exposed to gdscript, add more util functions

--- modules/spout/spout_gd.cpp
+++ modules/spout/spout_gd.cpp
@@ -1,62 +1,181 @@
 #include "spout_gd.h"
 
-void SpoutGD::send(Ref<Image> image) {
-    PoolVector<uint8_t>::Read data = image->get_data().read();
-    const unsigned char *p = (const unsigned char *)data.ptr();
-    sender->SendImage(p, sender_width, sender_height);
+Array Spout::get_senders() {
+	Array arr;
+
+	int count = spout_lib->GetSenderCount();
+	arr.resize(count);
+
+	for (int i = 0; i < count; i++) {
+		char sender_name[256];
+
+		spout_lib->GetSender(i, sender_name);
+
+		arr.append(String(sender_name));
+	}
+
+	return arr;
 }
 
-void SpoutGD::set_size(int width, int height) {
-    sender_width = width;
-    sender_height = height;
+String Spout::get_active_sender() {
+	char sender_name[256];
+
+	return spout_lib->GetActiveSender(sender_name) ? String(sender_name) : String();
 }
 
-void SpoutGD::set_rot(int x, int y) {
-    rot_x = x;
-    rot_y = y;
+Error Spout::set_active_sender(const String p_sender_name) {
+	return spout_lib->SetActiveSender(p_sender_name.utf8().get_data()) ? Error::OK : Error::FAILED;
 }
 
-double_t SpoutGD::get_fps() {
-    return sender->GetFps();
+Error Spout::send_image(Ref<Image> p_image) {
+	PoolVector<uint8_t>::Read data = p_image->get_data().read();
+	const unsigned char *p = (const unsigned char *)data.ptr();
+	return spout_lib->SendImage(p, sender_width, sender_height) ? Error::OK : Error::FAILED;
 }
 
-double_t SpoutGD::get_frame() {
-    return (double_t)sender->GetFrame();
+Error Spout::send_fbo(const uint32_t p_fbo_id) {
+	return spout_lib->SendFbo(p_fbo_id, sender_width, sender_height) ? Error::OK : Error::FAILED;
 }
 
-void SpoutGD::set_frame_count(bool enabled) {
-    sender->SetFrameCount(enabled);
+Error Spout::send_texture(const uint32_t p_shared_texture_id, const uint32_t p_godot_texture_id) {
+	return spout_lib->SendTexture(p_shared_texture_id, p_godot_texture_id, sender_width, sender_height) ? Error::FAILED : Error::OK;
 }
 
-bool SpoutGD::is_frame_count_enabled() {
-    return sender->IsFrameCountEnabled();
+void Spout::set_receiver_name(const String p_sender_name) {
+	spout_lib->SetReceiverName(p_sender_name.empty() ? nullptr : p_sender_name.utf8().get_data());
 }
 
-void SpoutGD::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("send", "image"), &SpoutGD::send);
-    ClassDB::bind_method(D_METHOD("set_size", "width", "height"), &SpoutGD::set_size);
-    ClassDB::bind_method(D_METHOD("set_rot", "x", "y"), &SpoutGD::set_rot);
-    ClassDB::bind_method(D_METHOD("get_fps"), &SpoutGD::get_fps);
-    ClassDB::bind_method(D_METHOD("get_frame"), &SpoutGD::get_frame);
-    ClassDB::bind_method(D_METHOD("set_frame_count", "enabled"), &SpoutGD::set_frame_count);
-    ClassDB::bind_method(D_METHOD("is_frame_count_enabled"), &SpoutGD::is_frame_count_enabled);
+void Spout::release_receiver() {
+	spout_lib->ReleaseReceiver();
 }
 
-SpoutGD::SpoutGD() {
-    strcpy_s(sender_name, 256, "SpoutLibrary Sender - GD");
-    
-    sender = GetSpout();
-    sender->EnableSpoutLog();
-    sender->SetSpoutLogLevel(SPOUT_LOG_VERBOSE);
-
-    rot_x = 0.0f;
-    rot_y = 0.0f;
-    sender_width = 100.0f;
-    sender_height = 100.0f;
-    sender->SetSenderName(sender_name);
+Error Spout::receive_texture(int p_shared_texture_id, int p_godot_texture_id, bool p_invert, int p_host_fbo) {
+	return spout_lib->ReceiveTexture((uint32_t)p_shared_texture_id, (uint32_t)p_godot_texture_id, p_invert, p_host_fbo) ? Error::OK : Error::FAILED;
 }
 
-SpoutGD::~SpoutGD() {
-    sender->ReleaseSender();
-    sender->Release();
+bool Spout::is_updated() {
+	return spout_lib->IsUpdated();
+}
+
+bool Spout::is_connected() {
+	return spout_lib->IsConnected();
+}
+
+bool Spout::is_frame_new() {
+	return spout_lib->IsFrameNew();
+}
+
+String Spout::get_sender_name() {
+	const char *sender_name = spout_lib->GetSenderName();
+
+	return sender_name ? String(sender_name) : String();
+}
+
+Vector2 Spout::get_sender_size() {
+	return Vector2((real_t)spout_lib->GetSenderWidth(), (real_t)spout_lib->GetSenderHeight());
+}
+
+double_t Spout::get_sender_fps() {
+	return spout_lib->GetSenderFps();
+}
+
+double_t Spout::get_sender_frame() {
+	return (double_t)spout_lib->GetSenderFrame();
+}
+
+bool Spout::get_sender_cpu() {
+	return spout_lib->GetSenderFrame();
+}
+
+bool Spout::get_sender_gldx() {
+	return spout_lib->GetSenderGLDX();
+}
+
+bool Spout::is_initialized() {
+	return spout_lib->IsInitialized();
+}
+
+bool Spout::bind_shared_texture() {
+	return spout_lib->BindSharedTexture();
+}
+
+bool Spout::unbind_shared_texture() {
+	return spout_lib->UnBindSharedTexture();
+}
+
+uint32_t Spout::get_shared_texture_id() {
+	return (int)spout_lib->GetSharedTextureID();
+}
+
+void Spout::set_size(int p_width, int p_height) {
+	sender_width = p_width;
+	sender_height = p_height;
+}
+
+double_t Spout::get_fps() {
+	return spout_lib->GetFps();
+}
+
+double_t Spout::get_frame() {
+	return (double_t)spout_lib->GetFrame();
+}
+
+bool Spout::get_cpu() {
+	return spout_lib->GetCPU();
+}
+
+bool Spout::get_gldx() {
+	return spout_lib->GetGLDX();
+}
+
+void Spout::set_frame_count(bool p_enabled) {
+	spout_lib->SetFrameCount(p_enabled);
+}
+
+bool Spout::is_frame_count_enabled() {
+	return spout_lib->IsFrameCountEnabled();
+}
+
+void Spout::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_senders"), &Spout::get_senders);
+	ClassDB::bind_method(D_METHOD("get_active_sender"), &Spout::get_active_sender);
+	ClassDB::bind_method(D_METHOD("set_active_sender"), &Spout::set_active_sender);
+
+	ClassDB::bind_method(D_METHOD("send_image", "image"), &Spout::send_image);
+	ClassDB::bind_method(D_METHOD("send_fbo", "fbo_id"), &Spout::send_fbo);
+	ClassDB::bind_method(D_METHOD("send_texture", "shared_texture_id", "godot_texture_id"), &Spout::send_texture);
+
+	// TODO add receiver methods
+
+	ClassDB::bind_method(D_METHOD("is_initialized"), &Spout::is_initialized);
+	ClassDB::bind_method(D_METHOD("bind_shared_texture"), &Spout::bind_shared_texture);
+	ClassDB::bind_method(D_METHOD("unbind_shared_texture"), &Spout::unbind_shared_texture);
+	ClassDB::bind_method(D_METHOD("get_shared_texture_id"), &Spout::get_shared_texture_id);
+
+	ClassDB::bind_method(D_METHOD("set_size", "width", "height"), &Spout::set_size);
+
+	ClassDB::bind_method(D_METHOD("get_fps"), &Spout::get_fps);
+	ClassDB::bind_method(D_METHOD("get_frame"), &Spout::get_frame);
+	ClassDB::bind_method(D_METHOD("get_cpu"), &Spout::get_cpu);
+	ClassDB::bind_method(D_METHOD("get_gldx"), &Spout::get_gldx);
+
+	ClassDB::bind_method(D_METHOD("set_frame_count", "enabled"), &Spout::set_frame_count);
+	ClassDB::bind_method(D_METHOD("is_frame_count_enabled"), &Spout::is_frame_count_enabled);
+}
+
+Spout::Spout() {
+	strcpy_s(sender_name, 256, "SpoutLibrary Sender - GD");
+
+	spout_lib = GetSpout();
+	spout_lib->EnableSpoutLog();
+	spout_lib->SetSpoutLogLevel(SPOUT_LOG_VERBOSE);
+
+	sender_width = 100.0f;
+	sender_height = 100.0f;
+	spout_lib->SetSenderName(sender_name);
+}
+
+Spout::~Spout() {
+	spout_lib->ReleaseSender();
+	spout_lib->Release();
 }
--- modules/spout/spout_gd.h
+++ modules/spout/spout_gd.h
@@ -1,39 +1,69 @@
 #ifndef SPOUT_GD_H
 #define SPOUT_GD_H
 
+#include "core/array.h"
+#include "core/error_list.h"
+#include "core/math/vector2.h"
 #include "core/reference.h"
 #include "scene/resources/texture.h"
 
 #include "thirdparty/SPOUTSDK/SpoutLibrary/Binaries/x64/SpoutLibrary.h"
 
-class SpoutGD : public Reference {
-    GDCLASS(SpoutGD, Reference);
+class Spout : public Reference {
+	GDCLASS(Spout, Reference);
 
 private:
-    SPOUTLIBRARY *sender;
-    char sender_name[256];
-    uint32_t sender_width;
-    uint32_t sender_height;
-
-    float rot_x, rot_y;
+	SPOUTLIBRARY *spout_lib;
+	char sender_name[256];
+	uint32_t sender_width;
+	uint32_t sender_height;
 
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 public:
-    void send(Ref<Image> image);
-    
-    void set_size(int width, int height);
-    void set_rot(int x, int y);
+	// Sender names
+	Array get_senders();
+	String get_active_sender();
+	Error set_active_sender(const String p_sender_name);
 
-    double_t get_fps();
-    double_t get_frame();
+	// Sender
+	Error send_image(Ref<Image> p_image);
+	Error send_fbo(const uint32_t p_fbo_id);
+	Error send_texture(const uint32_t p_shared_texture_id, const uint32_t p_godot_texture_id);
 
-    void set_frame_count(bool enabled);
-    bool is_frame_count_enabled();
+	// Receiver
+	void set_receiver_name(const String p_sender_name);
+	void release_receiver();
+	Error receive_texture(int p_shared_texture_id, int p_godot_texture_id, bool p_invert, int p_host_fbo);
+	bool is_updated();
+	bool is_connected();
+	bool is_frame_new();
+	String get_sender_name();
+	Vector2 get_sender_size();
+	double_t get_sender_fps();
+	double_t get_sender_frame();
+	bool get_sender_cpu();
+	bool get_sender_gldx();
 
-    SpoutGD();
-    ~SpoutGD();
+	// OpenGL shared texture
+	bool is_initialized();
+	bool bind_shared_texture();
+	bool unbind_shared_texture();
+	uint32_t get_shared_texture_id();
+
+	void set_size(int p_width, int p_height);
+
+	double_t get_fps();
+	double_t get_frame();
+	bool get_cpu();
+	bool get_gldx();
+
+	void set_frame_count(bool p_enabled);
+	bool is_frame_count_enabled();
+
+	Spout();
+	~Spout();
 };
 
 #endif
