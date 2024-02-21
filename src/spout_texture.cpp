897c214aa027c74ebac8cf90eca2bc77ff4acbfe
Erodozer <ero@erodozer.moe>
2024-02-21 18:41:34 -0500

    reuse image buffer for writing
    try to decrease allocations per frame and lower memory usage

--- src/spout_texture.cpp
+++ src/spout_texture.cpp
@@ -11,23 +11,31 @@ void SpoutTexture::set_sender_name(String sender_name) {
 
     _spout->release_receiver();
     _spout->set_receiver_name(sender_name);
+    godot::UtilityFunctions::print("[spout-gd] connecting to sender ", sender_name);
 }
 
 String SpoutTexture::get_sender_name() const {
     return _sender_name;
 }
 
-void SpoutTexture::rebuild_image() {
-    int32_t width = _spout->get_sender_width();
-    int32_t height = _spout->get_sender_height();
-
+void SpoutTexture::rebuild_image(int32_t width, int32_t height) {
     if (width <= 0 || height <= 0) {
         return;
     }
 
-    _image = Image::create(width, height, false, Image::Format::FORMAT_RGBA8);
+    godot::UtilityFunctions::print("[spout-gd] image dimensions changed ", width, "x", height);
 
+    // keep an internal reference to the mutable image
+    // godot's get_image function returns a copy from the GPU, so it's not very performant
+    _image = Image::create(width, height, false, Image::Format::FORMAT_RGBA8);
+    
     ImageTexture::set_image(_image);
+    
+    // Godot creates a copy of the data buffer as well because it's safer and prevents unintentional
+    // updates and race conditions.  However, this is a controlled scenario and we
+    // explicitly do want to update the data, so let's make this as performant as possible.
+    // We do this be reusing a shared buffer associated by reference.
+    _buffer = _image->get_data();
 }
 
 void SpoutTexture::poll_server() {
@@ -35,18 +43,24 @@ void SpoutTexture::poll_server() {
         return;
     }
 
-    String active_name = _spout->get_sender_name();
-    if (active_name != _sender_name) {
-        set_sender_name(active_name);
-    }
-
     // detect if source info has changed
 	// Spout SDK states that this is required before every receive call
     if (_spout->is_updated()) {
-        rebuild_image();
+        int32_t width = _spout->get_sender_width();
+        int32_t height = _spout->get_sender_height();
+        
+        rebuild_image(width, height);
     }
-
-    if (_spout->receive_image(_image, Spout::GLFormat::FORMAT_RGBA, false, false)) {
+    
+    if (_spout->receive_buffer(_buffer, Spout::GLFormat::FORMAT_RGBA, false, false)) {
+        _image->set_data(
+            _image->get_width(),
+            _image->get_height(),
+            _image->has_mipmaps(),
+            _image->get_format(),
+            _buffer
+        );
+        // tells texture storage that the buffer associated with this image has been updated
         ImageTexture::update(_image);
     }
 }
@@ -54,12 +68,13 @@ void SpoutTexture::poll_server() {
 SpoutTexture::SpoutTexture() {
     // create a placeholder image for spout
     _spout = new Spout();
-    _image = Image::create(int32_t(100), int32_t(100), false, Image::Format::FORMAT_RGBA8);
     _sender_name = String("");
+    _image = Image::create(int32_t(1), int32_t(1), false, Image::Format::FORMAT_RGBA8);
+    _buffer = _image->get_data();
     ImageTexture::set_image(_image);
 
     RenderingServer::get_singleton()->texture_set_force_redraw_if_visible(get_rid(), true);
-
+    
     auto _update = callable_mp(this, &SpoutTexture::poll_server);
 
     RenderingServer::get_singleton()->connect(
--- src/spout_texture.h
+++ src/spout_texture.h
@@ -2,11 +2,13 @@
 #define SPOUT_GD_SPOUT_TEXTURE_H
 
 #include <godot_cpp/classes/global_constants.hpp>
+#include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/image.hpp>
 #include <godot_cpp/classes/image_texture.hpp>
 
 #include <godot_cpp/classes/rendering_server.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/rid.hpp>
 #include <godot_cpp/variant/variant.hpp>
 
 #include "spout_gd.h"
@@ -15,13 +17,14 @@ using namespace godot;
 
 class SpoutTexture : public ImageTexture {
     GDCLASS(SpoutTexture, ImageTexture);
-
+	
     private:
         Spout *_spout;
         Ref<Image> _image;
+        PackedByteArray _buffer;
         String _sender_name;
 
-        void rebuild_image();
+        void rebuild_image(int32_t width, int32_t height);
         void poll_server();
     protected:
         static void _bind_methods();
